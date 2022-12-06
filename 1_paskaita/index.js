d1d2be8b188d4b0bc96f285ecd999411cde36fa3
Rokas Andreikenas <rokas.andreikenas@exacaster.com>
2022-12-06 18:42:24 +0200

    Add more examples for solution

--- 1_paskaita/index.js
+++ 1_paskaita/index.js
@@ -1,3 +1,19 @@
 const casual = require("casual");
 
 console.log(casual.city);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.trunc(Math.random() * 10) + 1);
+console.log(casual.integer(1, 10));
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+console.log(randomNumber(1, 10));
+
+const name = "Rokas";
+const surname = "Andreikenas";
+const fullName = name + " " + surname; // bad
+const newFullName = `${name} ${surname}`; // good
+
+console.log(`${casual.name_prefix} ${casual.first_name} ${casual.last_name}`);

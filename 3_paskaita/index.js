9d07c03e8caf7113cc5dbceb8638c44653295a91
Rokas Andreikenas <rokas.andreikenas@exacaster.com>
2022-12-07 20:47:51 +0200

    Add GET and POST methods with error handling

---
+++ 3_paskaita/index.js
@@ -0,0 +1,59 @@
+// npm init - sukuria package.json faila
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json()); // is JSON i JS
+
+const cars = [
+  {
+    id: 1,
+    make: "BMW",
+    model: "530",
+    color: "Black",
+  },
+  {
+    id: 2,
+    make: "Audi",
+    model: "A6",
+    color: "White",
+  },
+];
+
+app.get("/cars", (req, res) => {
+  res.send(cars);
+});
+
+app.get("/cars/:id", (req, res) => {
+  const id = +req.params.id;
+  const car = cars.find((car) => car.id === id);
+
+  if (car) {
+    res.send(car);
+  } else {
+    res.status(404).send({
+      error: "Car not found",
+    });
+  }
+});
+
+// atsiuncia {make, model, color}
+// gauna {id, make, model, color}
+app.post("/cars", (req, res) => {
+  const car = req.body;
+  if (car.make && car.model && car.color) {
+    const newCar = { ...car, id: Date.now() };
+    cars.push(newCar);
+    res.send(newCar);
+  } else {
+    res.status(400).send({
+      error: "Invalid request",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on the ${port}`);
+});

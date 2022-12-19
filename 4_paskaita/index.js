633d1f78e14d0db2e777a2d4b93acc716db1a66a
Rokas Andreikenas <shyptt@gmail.com>
2022-12-19 18:43:27 +0200

    Add multiple query params example

--- 4_paskaita/index.js
+++ 4_paskaita/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -13,9 +13,28 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
+  const { brand, sort, property } = req.query;
+
   try {
+    const con = await client.connect();
+    const data = await con
+      .db('first')
+      .collection('cars')
+      .find(brand ? { $or: [{ brand: { $in: brand.split(',') } }] } : {})
+      .sort(sort ? { [property]: sort === 'asc' ? 1 : -1 } : {})
+      .toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
     const con = await client.connect(); // prisijungimas prie duomenu bazes
-    const data = await con.db('first').collection('cars').find().toArray(); // duomenu istraukimas
+    const data = await con.db('first').collection('cars').findOne(ObjectId(id));
     await con.close(); // prisijungimo isjungimas
     res.send(data);
   } catch (error) {
@@ -29,7 +48,7 @@ app.post('/', async (req, res) => {
     const data = await con
       .db('first')
       .collection('cars')
-      .insertOne({ brand: 'VW', model: 'Polo' });
+      .insertOne({ brand: 'Seat', model: 'Ibiza' });
     await con.close();
     res.send(data);
   } catch (error) {
